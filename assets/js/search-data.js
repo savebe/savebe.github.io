1b9c003c0d120367e65e69690d44804572081ead
savebe <samuel.benz@hispeed.ch>
2026-08-30 14:08:22 +0000

    Deploying to gh-pages from @ savebe/savebe.github.io@d9dc5580992015b740fa0b817c714b846f6d403c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,9 +30,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "projects-axi5-orbit-5-axis-fdm-printer",
-          title: 'AXI5 ORBIT - 5-Axis FDM Printer',
-          description: "A functional 5-axis FDM printer prototype — CAD, mechatronics, firmware and validation (bachelor thesis)",
+        },{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/axi5-orbit-project/";
             },},{id: "projects-pipeguard-robotics",
